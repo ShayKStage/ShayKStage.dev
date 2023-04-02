67ef814a1ec97792fc2a6274d38107a37db4c60c
314ShadePi <erikkirkegaardstage@gmail.com>
2023-04-02 17:18:24 +0200

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), vue(), solidJs(), sitemap(), prefetch(), markdoc()]
+});
